d776ea27fddf400d150d85401d48dcc2ee54ee82
Sum1Solutions <17915475+Sum1Solutions@users.noreply.github.com>
2023-07-12 10:28:34 -0400

    many changes

--- ask.sh
+++ ask.sh
@@ -1,10 +1,26 @@
 #!/bin/zsh
 
+# Set log file path
+LOG_FILE_PATH="/Users/jb/coding/ask_openAI_cli/conversation.log"
+
+# Check for flags
+if [[ $1 == "-c" ]] || [[ $1 == "-x" ]]
+then
+    flag=$1
+    shift
+
+    case "${flag}"
+    in
+    -c) cat $LOG_FILE_PATH;;  # If -c, print the log file
+    -x) rm $LOG_FILE_PATH;;  # If -x, delete the log file
+    esac
+fi
+
 # Activate the virtual environment
-source /Users/jb/coding/ask_openAI_cli/env/bin/activate
+#source /Users/jb/coding/ask_openAI_cli/env/bin/activate
 
 # Execute the Python script within the virtual environment
-python /Users/jb/coding/ask_openAI_cli/chatbot.py "$*"
+python3 /Users/jb/coding/ask_openAI_cli/chatbot.py "$*"
 
 # Deactivate the virtual environment (but lose all context of prior conversation)
 # deactivate
--- chatbot.py
+++ chatbot.py
@@ -6,56 +6,90 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
-# Set up OpenAI API client
+# Set OpenAI API key
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-# Define conversation history
-conversation = []
+# Set the log file path
+LOG_FILE_PATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), "conversation.log")
 
-# Load conversation history from the log file
-log_file = os.path.join(os.path.dirname(os.path.realpath(__file__)), "conversation.log")
-if os.path.exists(log_file):
-    with open(log_file, "r") as f:
-        conversation_lines = f.read().splitlines()
-        conversation = [{"role": "user" if line.split(": ")[0] == "User" else "assistant", 
-                         "content": ": ".join(line.split(": ")[1:])} 
-                        for line in conversation_lines]
-else:
-    conversation = [{"role": "assistant", "content": "Hello! How can I assist you today?"}]
+def sanitize_input(input_string):
+    """Sanitize the input string by removing certain special characters."""
+    
+    # Define characters to remove
+    chars_to_remove = ['?', "'"]
+    
+    # Remove defined characters from the input string
+    sanitized_string = ''.join([char for char in input_string if char not in chars_to_remove])
+    
+    return sanitized_string
 
+def load_conversation():
+    """Load conversation history from the log file."""
+    
+    # Check if the log file exists
+    if os.path.exists(LOG_FILE_PATH):
+        # If the log file exists, read it and construct the conversation history
+        with open(LOG_FILE_PATH, "r") as f:
+            conversation_lines = f.read().splitlines()
+            return [{"role": "user" if line.split(": ")[0] == "User" else "assistant", 
+                     "content": ": ".join(line.split(": ")[1:])} 
+                    for line in conversation_lines]
+    else:
+        # If the log file does not exist, start a new conversation
+        return [{"role": "assistant", "content": "Hello! How can I assist you today?"}]
 
-def send_message(message):
-    # Add the user's message to the conversation
-    conversation.append({"role": "user", "content": message})
-
-    # Start messages with the system message
-    messages = [{"role": "system", "content": "You are a helpful assistant."}]
-
-    # Add the rest of the conversation to messages
-    messages.extend(conversation)
-
-    # Send the messages to the API
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",  # You may change this to text-davinci-003 if you prefer
-        messages=messages
-    )
-
-    # Get the assistant's response
-    assistant_message = response['choices'][0]['message']['content']
-
-    # Add the assistant's message to the conversation
-    conversation.append({"role": "AI", "content": assistant_message})
-
-    # Save the updated conversation history to the log file
-    with open(log_file, "w") as f:
+def write_to_log(conversation):
+    """Write the conversation history to the log file."""
+    
+    with open(LOG_FILE_PATH, "w") as f:
         f.write("\n".join([f"{item['role']}: {item['content']}" for item in conversation]))
 
-    # Return the assistant's message
+def send_message(conversation, message):
+    """Send a message to the OpenAI API and receive the assistant's response."""
+    
+    # Add the user's message to the conversation history
+    conversation.append({"role": "user", "content": message})
+    
+    # Construct the messages to be sent to the API
+    messages = [{"role": "system", "content": "You are a helpful assistant."}]
+    messages.extend(conversation)
+    
+    # Send the messages to the API
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",  # Change this to another model if you prefer
+        messages=messages
+    )
+    
+    # Extract the assistant's response
+    assistant_message = response['choices'][0]['message']['content']
+    
+    # Add the assistant's response to the conversation history
+    conversation.append({"role": "AI", "content": assistant_message})
+    
+    # Write the updated conversation history to the log file
+    write_to_log(conversation)
+    
+    # Return the assistant's response
     return assistant_message
 
-# Get user's message from command-line argument
-user_message = " ".join(sys.argv[1:])
+def main():
+    """Main function to handle the user's message and print the assistant's response."""
+    
+    # Load the conversation history
+    conversation = load_conversation()
+    
+    # Get the user's message from the command-line arguments
+    user_message = " ".join(sys.argv[1:])
+    
+    # Sanitize the user's message
+    sanitized_user_message = sanitize_input(user_message)
+    
+    # Send the sanitized user's message to the chatbot and get the response
+    response = send_message(conversation, sanitized_user_message)
+    
+    # Print the response
+    print(response)
 
-# Send the user's message to the chatbot and print the response
-response = send_message(user_message)
-print(response)
+# Run the main function if the script is run directly
+if __name__ == "__main__":
+    main()
